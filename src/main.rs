64784207e16f99522be7917f62cafb1fd22a3e7e
marton <marton@near.st>
2021-12-01 23:13:18 +0000

    day 1/part 2

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,28 @@ fn main() {
         .map(|l| l.unwrap().parse::<i32>().unwrap())
         .collect();
     
-    // part 1
-    let mut prev: i32 = lines[0];
-    let mut counter = 0;
-    for i in 1..lines.len() {
-        let current = lines[i];
+    // // part 1
+    // let mut prev: i32 = lines[0];
+    // let mut counter = 0;
+    // for i in 1..lines.len() {
+    //     let current = lines[i];
 
-        if current > prev {
-            counter = counter + 1;
+    //     if current > prev {
+    //         counter = counter + 1;
+    //     }
+    //     prev = current;    
+    // }
+    // println!("{}", counter);
+
+    //part 2
+    let mut prev_sum_of_window = lines[0] + lines[1] + lines[2];
+    let mut new_counter = 0;
+    for n in 1..lines.len() - 2 {
+        let sum_of_window = lines[n] + lines[n + 1] + lines[n + 2];
+        if sum_of_window > prev_sum_of_window {
+            new_counter = new_counter + 1;
         }
-        prev = current;    
+        prev_sum_of_window = sum_of_window;
     }
-    println!("{}", counter);
+    println!("{}", new_counter);
 }
