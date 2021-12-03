7666e19095660678794fca871e4827566049cc41
marton <marton@near.st>
2021-12-03 20:39:42 +0000

    day 3, part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,10 +1,40 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+fn reduce(ls: &mut [String], pos: usize, criteria: String) -> String {
+    if ls.len() == 1 {
+        return ls.first().unwrap().to_string();
+    }
+
+    let mut ones: Vec<String> = Vec::new();
+    let mut zeros: Vec<String> = Vec::new();
+    for i in 0..ls.len() {
+        if ls[i].chars().nth(pos).unwrap() == '1' {
+            ones.push(ls[i].clone());
+        } else {
+            zeros.push(ls[i].clone());
+        }
+    }
+
+    if criteria == "most_common" {
+        if ones.len() >= zeros.len() {
+            return reduce(&mut ones, pos + 1, criteria);
+        } else {
+            return reduce(&mut zeros, pos + 1, criteria);
+        };
+    } else {
+        if ones.len() < zeros.len() {
+            return reduce(&mut ones, pos + 1, criteria);
+        } else {
+            return reduce(&mut zeros, pos + 1, criteria);
+        };
+    }
+}
+
 fn main() {
     let file = File::open("src/input.txt").expect("no such file");
     let buf = BufReader::new(file);
-    let lines: Vec<String> = buf.lines().map(|l| l.unwrap()).collect();
+    let mut lines: Vec<String> = buf.lines().map(|l| l.unwrap()).collect();
 
     let length = lines[0].len();
     let mut dir = vec![0; length];
@@ -33,11 +63,21 @@ fn main() {
 
     let gamma = isize::from_str_radix(&gamma_bin, 2).unwrap();
     let epsilon = isize::from_str_radix(&epsilon_bin, 2).unwrap();
-    
     println!(
-        "PART 1. Gamma rate: {}, Epsilon rate: {}, Power: {}",
+        "PART 1. Gamma rate: {}, Epsilon rate: {}, Power consumption: {}",
         gamma,
         epsilon,
         gamma * epsilon
+    );
+
+    let oxygen_gen_bin = reduce(&mut lines, 0, "most_common".to_string());
+    let co2_scrubber_bin = reduce(&mut lines, 0, "least_common".to_string());
+
+    let oxygen_gen = isize::from_str_radix(&oxygen_gen_bin, 2).unwrap();
+    let co2_scrubber = isize::from_str_radix(&co2_scrubber_bin, 2).unwrap();
+
+    println!(
+        "PART 2. Oxygen generator: {}, CO2 scrubber: {}, Life support: {}",
+        oxygen_gen, co2_scrubber, oxygen_gen * co2_scrubber
     );
 }
